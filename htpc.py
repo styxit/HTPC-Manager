fdc0fae28b24ee2274bef81d8fdcf3a58bb767dd
Michael Winkel <mbw@mbw.dk>
2012-06-13 13:36:09 +0200

    modified:   .gitignore 	modified:   config-sample.cfg 	modified:   htpc.py

--- htpc.py
+++ htpc.py
@@ -4,7 +4,7 @@ import argparse
 from htpc.serve import serve
 
 # Default configuration file
-config = os.path.join(os.getcwd(), 'userdata/config.cfg')
+config = os.path.join(os.getcwd(), 'config.cfg')
 
 # Get variables from commandline
 parser = argparse.ArgumentParser()
@@ -12,8 +12,8 @@ parser.add_argument('-c', '--config', default=config)
 parser.add_argument('-d', '--daemon', action='store_true', default=0)
 args = parser.parse_args()
 
-if not os.path.isfile(config):
-    sys.exit("Configuration file: '+configfile+' doesn't exist!")
+if not os.path.isfile(args.config):
+    sys.exit("Configuration file: "+args.config+" doesn't exist. Copy config-sample.cfg to config.cfg")
 
 if args.daemon and sys.platform == 'win32':
     print "Daemon mode not possible on Windows. Starting normally"
