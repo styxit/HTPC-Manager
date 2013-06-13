7a0b529ee3a2308766fe75b749dc6c938d0b0210
Floris <info@styxit.com>
2013-06-14 00:30:20 +0200

    XbmcImageProxy use jpg
    Use jpeg images instead of png. Png alpha channel is not actually used.
    jpeg is smaller in filesize, improving response time. Close #21

--- htpc/proxy.py
+++ htpc/proxy.py
@@ -26,7 +26,7 @@ def get_image(url, height=None, width=None, opacity=100, auth=None):
     image = os.path.join(imgdir, imghash)
 
     # If there is no local copy of the original
-    if not os.path.isfile(image + '.png'):
+    if not os.path.isfile(image + '.jpg'):
         download_image(url, image, auth)
 
     # Check if resize is needed
@@ -34,13 +34,13 @@ def get_image(url, height=None, width=None, opacity=100, auth=None):
         # Set filename for resized file
         resized = image + '_w' + width + '_h' + height + '_o' + str(opacity)
         # If there is no local resized copy
-        if not os.path.isfile(resized + '.png'):
+        if not os.path.isfile(resized + '.jpg'):
             resize_image(image, height, width, opacity, resized)
         # Serve the resized file
         image = resized
 
     # Load file from disk
-    return serve_file(path=image + '.png', content_type='image/png')
+    return serve_file(path=image + '.jpg', content_type='image/jpeg')
 
 
 def download_image(url, dest, auth=None):
@@ -50,16 +50,23 @@ def download_image(url, dest, auth=None):
         request.add_header("Authorization", "Basic %s" % auth)
     data = StringIO(urlopen(request).read())
     im = Image.open(data)
-    im.save(dest + '.png', 'PNG')
+    im.save(dest + '.jpg', 'JPEG', quality=95)
 
 
 def resize_image(img, height, width, opacity, dest):
     """ Resize image, set opacity and save to disk """
     size = int(width), int(height)
-    enhance = float(opacity) / 100
-    im = Image.open(img + '.png')
-    im = im.resize(size, Image.ANTIALIAS).convert('RGBA')
-    alpha = ImageEnhance.Brightness(im.split()[3]).enhance(enhance)
-    im.putalpha(alpha)
-    im.save(dest + '.png', 'PNG')
+    im = Image.open(img + '.jpg')
+    im = im.resize(size, Image.ANTIALIAS)
+
+    # Apply overlay if opacity is set
+    opacity = float(opacity)
+    if (opacity < 100):
+        enhance = opacity / 100
+        # Create white overlay image
+        overlay = Image.new('RGB', size, '#FFFFFF')
+        #apply overlay to resized image
+        im = Image.blend(overlay, im, enhance)
+
+    im.save(dest + '.jpg', 'JPEG', quality=95)
     return dest
