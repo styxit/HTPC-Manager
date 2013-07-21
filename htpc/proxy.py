0c8b715dc18c1145d6bd2e21e00ff51c96a1320f
mbw2001 <mbw@mbw.dk>
2013-07-21 17:18:32 +0200

    More PEP8 Pillow support

--- htpc/proxy.py
+++ htpc/proxy.py
@@ -17,7 +17,6 @@ except ImportError:
         PIL = False
 
 
-
 def get_image(url, height=None, width=None, opacity=100, auth=None):
     """ Load image form cache if possible, else download. Resize if needed """
     opacity = float(opacity)
