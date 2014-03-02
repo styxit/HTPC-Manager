4b1db501c4ff3d226985c5109afdcbe1951c3e22
John <secretmail.com>
2014-03-02 22:08:27 +0100

    Fix css

--- interfaces/default/js/stats.js
+++ interfaces/default/js/stats.js
@@ -60,18 +60,6 @@ function get_diskinfo() {
 
             $.each(response, function (i, disk) {
                 var row = $('<tr>');
-                var progressBar = $('<div>');
-                progressBar.addClass('bar');
-                progressBar.css('width', (disk.percent) + '%');
-                progressBar.text(getReadableFileSizeString(disk.used)); // added the text
-                var progress = $('<div>');
-                progress.addClass('progress');
-
-                if (disk.percent >= 90) {
-                    progress.addClass('progress-danger');
-                }
-                progress.append(progressBar);
-
                 var progress2 = 	"<div class='progress hddprog'><div class=bar style=width:" + disk.percent + "%><span class=sr-only>"+ getReadableFileSizeStringHDD(disk.used) +"</span></div><div class='bar bar-success' style=width:" + (100 - disk.percent) + "% ><span class=sr-only>" + getReadableFileSizeStringHDD(disk.free) +"</span></div>";
 
                 if (disk.percent >=85) {
@@ -115,12 +103,12 @@ function get_local_ip() {
 // Not in use
 function network_usage() {
     $.getJSON(WEBDIR + "stats/network_usage", function (response) {
-	$(".nw").append("<div>Recv: "+ getReadableFileSizeString(response.bytes_recv) +"</div>");
-	$(".nw").append("<div>Sent: "+ getReadableFileSizeString(response.bytes_sent) +"</div>");
-	$(".nw").append("<div>Error in: "+ response.errin +"</div>");
-	$(".nw").append("<div>Error out: "+ response.errout +"</div>");
-	$(".nw").append("<div>Drop in: "+ response.dropin +"</div>");
-	$(".nw").append("<div>Drop out: "+ response.dropout +"</div>");
+        $(".nw").append("<div>Recv: "+ getReadableFileSizeString(response.bytes_recv) +"</div>");
+        $(".nw").append("<div>Sent: "+ getReadableFileSizeString(response.bytes_sent) +"</div>");
+        $(".nw").append("<div>Error in: "+ response.errin +"</div>");
+        $(".nw").append("<div>Error out: "+ response.errout +"</div>");
+        $(".nw").append("<div>Drop in: "+ response.dropin +"</div>");
+        $(".nw").append("<div>Drop out: "+ response.dropout +"</div>");
     });
 }
 
@@ -128,13 +116,13 @@ function network_usage_table() {
     $.getJSON(WEBDIR + "stats/network_usage", function (response) {
         $("#stat-sent").text(getReadableFileSizeString(response.bytes_sent));
         $("#stat-recv").text(getReadableFileSizeString(response.bytes_recv));
-        $(".nw").html("<table class='table nwtable networktable'><tr><td>Network</td><td>In</td><td>Out</td</tr><tr><td>Drop</td><td>" + response.dropin + "</td><td>" + response.dropout + "</td></tr><tr><td>Error</td><td>" + response.errin + "</td><td>" + response.errout + "</td></tr><tr><td>IP</td><td class=tlip></td><td class=txip></td></tr></tbody></table>");
+        $(".nw").html("<table class='table nwtable'><tr><td class=span4>Network</td><td class=span4>In</td><td class=span4>Out</td</tr><tr><td>Drop</td><td>" + response.dropin + "</td><td>" + response.dropout + "</td></tr><tr><td>Error</td><td>" + response.errin + "</td><td>" + response.errout + "</td></tr><tr><td>IP</td><td class=tlip></td><td class=txip></td></tr></tbody></table>");
     });
 }
 
 function get_user() {
     $.getJSON(WEBDIR + "stats/get_user", function (response) {
-	$("#system_user").text(response.name +" logged in " + response.started + " ago")
+        $("#system_user").text(response.name +" logged in " + response.started + " ago")
     });
 }
 
@@ -154,34 +142,32 @@ function virtual_memory_bar() {
 
 function virtual_memory_table() {
     $.getJSON(WEBDIR + "stats/virtual_memory", function (virtual) {
-	$(".virmem").html("<table class='table nwtable'><tr><td>Physical Memory</td><td>" + virtual.percent + "%</td></tr><tr><td>Total</td><td>" + getReadableFileSizeString(virtual.total) + "</td></tr><tr><td>Used</td><td>" + getReadableFileSizeString((virtual.total - virtual.available)) + "</td></tr><tr><td>Free</td><td>" + getReadableFileSizeString(virtual.available) + "</td></tr></tbody></table>");
+        $(".virmem").html("<table class='table nwtable'><tr><td class=span4>Physical Memory:</td><td class=span4>" + virtual.percent + "%</td></tr><tr><td>Total:</td><td>" + getReadableFileSizeString(virtual.total) + "</td></tr><tr><td>Used:</td><td>" + getReadableFileSizeString((virtual.total - virtual.available)) + "</td></tr><tr><td>Free:</td><td>" + getReadableFileSizeString(virtual.available) + "</td></tr></tbody></table>");
     });
 }
 
 function swap_memory_bar() {
     $.getJSON(WEBDIR + "stats/swap_memory", function (swap) {
-        //alert(response.total);
-	$(".swpmem").html("<div>Swap memory</div><div class=progress><div class=bar style=width:" + swap.percent + "%><span class=sr-only>Used: "+ swap.percent +"%</span></div><div class='bar bar-success' style=width:" + (100 - swap.percent) + "%><span class=sr-only>Free: " + (100 - swap.percent) +"%</span></div></div><div class=progress><div class=bar style=width:" + swap.percent + "%><span class=sr-only>Used: "+ getReadableFileSizeString(swap.used)+"</span></div><div class='bar bar-success' style=width:" + (100 - swap.percent) + "% ><span class=sr-only>Free: " + getReadableFileSizeString(swap.free) +"</span></div>");
-
+        $(".swpmem").html("<div>Swap memory</div><div class=progress><div class=bar style=width:" + swap.percent + "%><span class=sr-only>Used: "+ swap.percent +"%</span></div><div class='bar bar-success' style=width:" + (100 - swap.percent) + "%><span class=sr-only>Free: " + (100 - swap.percent) +"%</span></div></div><div class=progress><div class=bar style=width:" + swap.percent + "%><span class=sr-only>Used: "+ getReadableFileSizeString(swap.used)+"</span></div><div class='bar bar-success' style=width:" + (100 - swap.percent) + "% ><span class=sr-only>Free: " + getReadableFileSizeString(swap.free) +"</span></div>");
     });
 }
 
 function swap_memory_table() {
     $.getJSON(WEBDIR + "stats/swap_memory", function (swap) {
-        $(".swpmem").html("<table class='table nwtable'><tr><td>Swap Memory</td><td>" +swap.percent +"%</td></tr><tr><td>Total</td><td>"+getReadableFileSizeString(swap.total)+"</td></tr><tr><td>Used</td><td>"+getReadableFileSizeString(swap.used)+"</td></tr><tr><td>Free</td><td>"+getReadableFileSizeString(swap.free)+"</td></tr></tbody></table>");
+        $(".swpmem").html("<table class='table nwtable'><tr><td class=span4>Swap Memory:</td><td class=span4>" +swap.percent +"%</td></tr><tr><td>Total:</td><td>"+getReadableFileSizeString(swap.total)+"</td></tr><tr><td>Used:</td><td>"+getReadableFileSizeString(swap.used)+"</td></tr><tr><td>Free:</td><td>"+getReadableFileSizeString(swap.free)+"</td></tr></tbody></table>");
     });
 }
 
 
 function cpu_percent_bar() {
     $.getJSON(WEBDIR + "stats/cpu_percent", function (cpu) {
-        $(".cpu").html("<div>CPU</div><div class=progress><div class=bar style=width:" + (cpu.user + cpu.system).toFixed(1) + "%><span class=sr-only>Used: "+ (cpu.user + cpu.system).toFixed(1) +"%</span></div><div class='bar bar-success' style=width:" + (100 - (cpu.user + cpu.system)).toFixed(1) + "%><span class=sr-only>Idle: "+ cpu.idle.toFixed(1) +"%</span></div></div><div class=progress><div class=bar style=width:" + cpu.user.toFixed(1) + "%><span class=sr-only>User: "+ cpu.user.toFixed(1) +"%</span></div><div class='bar bar-warning' style=width:" + cpu.system.toFixed(1) + "%><span class=sr-only>System: "+ cpu.system.toFixed(1) +"%</span></div><div class='bar bar-success' style=width:" + (100 - (cpu.user + cpu.system)).toFixed(1) + "%><span class=sr-only>Idle: " + cpu.idle.toFixed(1) +"%</span></div></div>");
+        $(".cpu").html("<div class=text-center>CPU</div><div class=progress><div class=bar style=width:" + (cpu.user + cpu.system).toFixed(1) + "%><span class=sr-only>Used: "+ (cpu.user + cpu.system).toFixed(1) +"%</span></div><div class='bar bar-success' style=width:" + (100 - (cpu.user + cpu.system)).toFixed(1) + "%><span class=sr-only>Idle: "+ cpu.idle.toFixed(1) +"%</span></div></div><div class=progress><div class=bar style=width:" + cpu.user.toFixed(1) + "%><span class=sr-only>User: "+ cpu.user.toFixed(1) +"%</span></div><div class='bar bar-warning' style=width:" + cpu.system.toFixed(1) + "%><span class=sr-only>System: "+ cpu.system.toFixed(1) +"%</span></div><div class='bar bar-success' style=width:" + (100 - (cpu.user + cpu.system)).toFixed(1) + "%><span class=sr-only>Idle: " + cpu.idle.toFixed(1) +"%</span></div></div>");
     });
 }
 
 function cpu_percent_table() {
     $.getJSON(WEBDIR + "stats/cpu_percent", function (cpu) {
-        $(".cpu").html("<table class='table nwtable'><tr><td>CPU</td><td>" + (100 - cpu.idle).toFixed(1) + "%</td></tr><tr><td>User</td><td>" + cpu.user + "%</td></tr><tr><td>System</td><td>" + cpu.system + "%</td></tr><tr><td>Idle</td><td>" + cpu.idle + "%</td></tr></tbody></table>");
+        $(".cpu").html("<table class='table nwtable'><tr><td class=span4>CPU:</td><td class=span4>" + (100 - cpu.idle).toFixed(1) + "%</td></tr><tr><td>User:</td><td>" + cpu.user + "%</td></tr><tr><td>System:</td><td>" + cpu.system + "%</td></tr><tr><td>Idle:</td><td>" + cpu.idle + "%</td></tr></tbody></table>");
     });
 }
 
