4932f6c86796ac327d7572a6c752524d7e5948cc
John <secretmail.com>
2014-03-17 22:55:12 +0100

    f

--- interfaces/default/js/samsungtv.js
+++ interfaces/default/js/samsungtv.js
@@ -3,3 +3,10 @@
         $.get(WEBDIR + 'samsungtv/sendkey?action='+action);
         //alert('Done ', action)
     });
+    
+    $(document).on('click', '.send', function() {
+        var action = $('#msg').val();
+        $.get(WEBDIR + 'samsungtv/sendkey?action='+action);
+            alert('Done ', action);
+    });
+        
