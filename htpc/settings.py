8e8b44b4f8b18236d77bfdd9f8bc1c30fa13c1f6
Rocky Burt <rocky@serverzen.com>
2014-04-29 20:03:30 -0230

    Links can now use http_host, http_scheme, http_port in link url's

--- htpc/settings.py
+++ htpc/settings.py
@@ -6,6 +6,7 @@ import htpc
 import logging
 from sqlobject import connectionForURI, sqlhub, SQLObject, SQLObjectNotFound
 from sqlobject.col import StringCol
+import re
 
 
 class Setting(SQLObject):
@@ -83,5 +84,19 @@ class Settings:
 
     """ Get custom defined urls from database in json format """
     def getUrls(self):
-        links = self.get('custom_urls', '{}')
-        return loads(links)
+        linkstr = self.get('custom_urls', '{}')
+        links = loads(linkstr)
+
+        request = cherrypy.request
+        regex = r'([a-zA-Z]+)://([.a-zA-Z0-9_-]*)(:[0-9]*)?'
+        match = re.match(regex, request.base)
+        kwargs = {
+            'http_scheme': match.group(1),
+            'http_host': match.group(2),
+            'http_port': (match.group(3) or ':80')[1:],
+        }
+
+        for link in links:
+            link['url'] = link['url'] % kwargs
+
+        return links
