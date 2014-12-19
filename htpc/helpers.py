3b6645fcd1190c13dbde2469da212d1a50bd9aa1
John <hellowlol1@gmail.com>
2014-12-19 21:26:52 +0100

    fix logging in helpers

--- htpc/helpers.py
+++ htpc/helpers.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import logging
 
+logger = logging.getLogger('helpers')
+
 
 def create_https_certificates(ssl_cert, ssl_key):
     """
@@ -12,7 +14,7 @@ def create_https_certificates(ssl_cert, ssl_key):
         from lib.certgen import createKeyPair, createCertRequest, createCertificate, TYPE_RSA, \
             serial  # @UnresolvedImport
     except Exception, e:
-        #logger.log(u"pyopenssl module missing, please install for https access", logger.WARNING)
+        logger.error("pyopenssl module missing, please install for https access")
         return False
 
     # Create the CA Certificate
@@ -26,11 +28,12 @@ def create_https_certificates(ssl_cert, ssl_key):
     cert = createCertificate(req, (cacert, cakey), serial, (0, 60 * 60 * 24 * 365 * 10))  # ten years
 
     # Save the key and certificate to disk
+    # should save to userdata
     try:
         open(ssl_key, 'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey))
         open(ssl_cert, 'w').write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
-    except:
-        #logger.log(u"Error creating SSL key and certificate", logger.ERROR)
+    except Exception as e:
+        logger.error("Error creating SSL key and certificate %s" % e)
         return False
 
     return True
