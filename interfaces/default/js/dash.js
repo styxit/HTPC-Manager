8e2f8d87f46b1c2e482b8ef634444a71c7469431
John <hellowlol1@gmail.com>
2015-04-13 22:02:03 +0200

    mobile css stuff

--- interfaces/default/js/dash.js
+++ interfaces/default/js/dash.js
@@ -392,22 +392,25 @@ function loadsysinfo(options) {
         $('#dash_sysinfo_table_body').append(row);
     }).always(function() {
         $.getJSON(WEBDIR + 'vnstat/oneline', function(result) {
-            var bwtot = (parseFloat(result.download_speed, 10) + parseFloat(result.upload_speed, 10)).toFixed(2);
-            row = $('<tr>').addClass("hidden-phone");
-            row.append(
-                $('<td>').text("Speed"),
-                $('<td>').text('D: '+ result.download_speed),
-                $('<td>').text('U: '+ result.upload_speed),
-                $('<td>').text('T: '+ bwtot + ' kbits/s')
-            )
-            $('#dash_sysinfo_table_body').append(row);
-            row = $('<tr>').addClass("hidden-phone");
-            row.append(
-                $('<td>').text("BW"),
-                $('<td>').attr("colspan",3).html('<div>DL CM: ' + result.rx_current_month + '</div>' +
-                                                '<div>DL AT: ' + result.alltime_total_traffic + '</div>')
-            )
-            $('#dash_sysinfo_table_body').append(row);
+            if (result) {
+                var bwtot = (parseFloat(result.download_speed, 10) + parseFloat(result.upload_speed, 10)).toFixed(2);
+                row = $('<tr>').addClass("hidden-phone");
+                row.append(
+                    $('<td>').text("Speed"),
+                    $('<td>').text('D: '+ result.download_speed),
+                    $('<td>').text('U: '+ result.upload_speed),
+                    $('<td>').text('T: '+ bwtot + ' kbits/s')
+                )
+                $('#dash_sysinfo_table_body').append(row);
+                row = $('<tr>').addClass("hidden-phone");
+                row.append(
+                    $('<td>').text("BW"),
+                    $('<td>').attr("colspan",3).html('<div>DL CM: ' + result.rx_current_month + '</div>' +'<div>DL AT: ' + result.alltime_total_traffic + '</div>')
+                )
+                $('#dash_sysinfo_table_body').append(row);
+
+            }
+
         }).always(function() {
             end_refresh('sysinfo');
         });
