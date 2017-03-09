594581c6d701aa5acbfbec1683cd898f25f52e9a
Hellowlol <hellowlol1@gmail.com>
2017-03-10 00:21:37 +0100

    lock down plexpy module, fix reverse proxylink

--- modules/plexpy.py
+++ modules/plexpy.py
@@ -11,10 +11,11 @@ import htpc
 from htpc.helpers import get_image, striphttp, fix_basepath
 from cherrypy.lib.auth2 import require, member_of
 
+log = logging.getLogger('modules.plexpy')
+
 
 class Plexpy(object):
     def __init__(self, *args, **kwargs):
-        self.logger = logging.getLogger('modules.plexpy')
         htpc.MODULES.append({
             'name': 'Plexpy',
             'id': 'plexpy',
@@ -32,17 +33,22 @@ class Plexpy(object):
             ]
         })
         # Add endpoints
-        self._build_methods()
+        # self._build_methods()
 
     @cherrypy.expose()
     @require()
     def index(self):
         return htpc.LOOKUP.get_template('plexpy.html').render(scriptname='plexpy', webinterface=self._webinterface())
 
+    def ping(self, *args, **kwargs):
+        k = kwargs.copy()
+        k.update(dict(cmd='get_docs'))
+        return self._fetch(**k)
+
     def _webinterface(cls):
         url = Plexpy._build_url()
-        if htpc.settings.get('headphones_reverse_proxy_link'):
-            url = htpc.settings.get('headphones_reverse_proxy_link')
+        if htpc.settings.get('plexpy_reverse_proxy_link'):
+            url = htpc.settings.get('plexpy_reverse_proxy_link')
         return url
 
     @staticmethod
@@ -61,30 +67,29 @@ class Plexpy(object):
 
         return url
 
-    def _build_methods(cls):
-        """Helper to build endspoints like plexpy api"""
-        r = cls._fetch(**{'cmd': 'docs'})
-        if not r:
-            return
+    #def _build_methods(cls):
+    #    """Helper to build endspoints like plexpy api"""
+    #    r = cls._fetch(**{'cmd': 'docs'})
+    #    if not r:
+    #        return
 
-        def dummy(func_name='', *args, **kwargs):
-            d = {'cmd': func_name}
-            d.update(kwargs)
-            return json.dumps(cls._fetch(**d))
+    #    def dummy(func_name='', *args, **kwargs):
+    #        d = {'cmd': func_name}
+    #        d.update(kwargs)
+    #        return json.dumps(cls._fetch(**d))
 
-        for cmd, doc in r.items():
-            if cmd in []:
-                func = require(partial(dummy, cmd))
-            else:
-                func = partial(dummy, cmd)
+    #    for cmd, doc in r.items():
+    #        if cmd in []:
+    #            func = require(partial(dummy, cmd))
+    #        else:
+    #            func = partial(dummy, cmd)
 
-            setattr(cls, cmd, func)
-            f = getattr(cls, cmd)
-            # So the url is reachable
-            f.exposed = True
-
-        return r
-
+    #        setattr(cls, cmd, func)
+    #        f = getattr(cls, cmd)
+    #        # So the url is reachable
+    #        f.exposed = True
+    #
+    #    return r
 
     def _fetch(cls, *args, **kwargs):
         """Get stuff from plexpy and handles errors"""
@@ -104,4 +109,32 @@ class Plexpy(object):
             if resp.get('response', {}).get('result') == 'success':
                 return resp['response']['data']
         except:
+            log.exeption('Failed to get %s' % url)
             return
+
+    @cherrypy.expose()
+    @cherrypy.tools.json_out()
+    @require()
+    def get_history(self, *args, **kwargs):
+        log.debug('Fetching history')
+        k = kwargs.copy()
+        k.update(dict(cmd='get_history'))
+        return self._fetch(**k)
+
+    @cherrypy.expose()
+    @cherrypy.tools.json_out()
+    @require()
+    def get_activity(self, *args, **kwargs):
+        log.debug('Fetching activity')
+        k = kwargs.copy()
+        k.update(dict(cmd='get_activity'))
+        return self._fetch(**k)
+
+    @cherrypy.expose()
+    @cherrypy.tools.json_out()
+    @require()
+    def get_home_stats(self, *args, **kwargs):
+        log.debug('Fetching home stats')
+        k = kwargs.copy()
+        k.update(dict(cmd='get_home_stats'))
+        return self._fetch(**k)
